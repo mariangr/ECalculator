92dbfa8e70e714d06ab00c43f239d5d92d21ccdd
Marian Graurov <marian_graurov@abv.bg>
2015-05-21 00:19:31 +0300

    Initial commit

---
+++ ECalculator/ECalculator.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECalculator
+{
+    public class ECalculator
+    {
+        private List<BigInteger> results;
+        private int thread_number;
+        private int threads_count;
+        private int precision;
+        private bool quietMode;
+        private int iterations;
+        private string outputPath;
+
+        public ECalculator(List<BigInteger> results, int numberOfThreads, int threads_count, int itterations, int precision, bool quietMode, string outputPath)
+        {
+            this.results = results;
+            this.thread_number = numberOfThreads;
+            this.threads_count = threads_count;
+            this.precision = precision;
+            this.quietMode = quietMode;
+            this.iterations = itterations;
+            this.outputPath = outputPath;
+        }
+
+        public void run()
+        {
+            //Stopwatch watch = new Stopwatch();
+            //watch.Start();
+
+            BigInteger result = new BigInteger(0);
+            BigInteger number;
+
+            try
+            {
+                for (int i = thread_number; i < iterations; i += threads_count)
+                {
+                    number = new BigInteger(((3 * i) * (3 * i) + 1));
+                    BigInteger st = BigInteger.Pow(new BigInteger(10), precision);
+                    number = BigInteger.Multiply(number, st);
+                    number = BigInteger.Divide(number, factorial(3 * i));
+                    if (number.ToString() == "0")
+                    {
+                        break;
+                    }
+                    result = BigInteger.Add(result, number);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+
+            if (!quietMode)
+            {
+                BigInteger degree = BigInteger.Pow(new BigInteger(10), precision);
+                string devider = BigInteger.Divide(result, degree).ToString();
+                string remainder = BigInteger.Remainder(result, degree).ToString();
+                while (remainder.Length < precision)
+                {
+                    remainder = "0" + remainder;
+                }
+                string neper = string.Format("{0}.{1}", devider, remainder);
+                Console.WriteLine("Thread number: " + thread_number + "\nThread execution result: " + neper + "\n");
+
+                if (!string.IsNullOrEmpty(outputPath))
+                {
+                    Program.writeToFile("Thread number: " + thread_number + "\nThread execution result: " + neper + "\n");
+                }
+            }
+
+
+
+            results.Add(result);
+
+            //watch.Stop();
+            //Console.WriteLine("Thread " + thread_number + " : " + watch.ElapsedMilliseconds );
+        }
+
+        public static BigInteger factorial(int number)
+        {
+            BigInteger factorial = new BigInteger(1);
+
+            for (int i = 1; i <= number; i++)
+            {
+                factorial = BigInteger.Multiply(factorial, new BigInteger(i));
+            }
+
+            return factorial;
+        }
+    }
+
+
+}
---
+++ ECalculator/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ECalculator
+{
+    class Program
+    {
+        private static int precision;
+        private static int threads_count;
+        private static bool quietMode;
+        private static string filePath;
+        
+
+        private static bool validateInputArguments(String[] args)
+        {
+            quietMode = false;
+
+            filePath = "";
+            precision = 10000;
+            threads_count = 4;
+
+            if (args.Length > 0)
+            {
+                if (args[args.Length - 1].Equals("-q"))
+                {
+                    quietMode = true;
+                }
+            }
+
+            for (int i = 0; i < args.Length - 1; i += 2)
+            {
+                if (args[i].Equals("-p"))
+                {
+                    if (!int.TryParse(args[i + 1], out precision))
+                    {
+                        return false;
+                    }
+
+                }
+                else if (args[i].Equals("-t"))
+                {
+                    if (!int.TryParse(args[i + 1], out threads_count))
+                    {
+                        return false;
+                    }
+
+                }
+                else if (args[i].Equals("-o"))
+                {
+                    filePath = args[i + 1];
+                    if (!filePath.StartsWith("./"))
+                    {
+                        filePath = "./" + filePath;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            if (!validateInputArguments(args))
+            {
+                Console.WriteLine("Invalid input!");
+                return;
+            }
+
+            runCalculations();
+        }
+
+        public static void runCalculations()
+        {
+            List<BigInteger> results = new List<BigInteger>();
+            Thread[] threads = new Thread[threads_count];
+
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            for (int i = 0; i < threads_count; i++)
+            {
+                ECalculator erunnable = new ECalculator(results, i, threads_count, precision, precision, quietMode, filePath);
+                Thread t = new Thread(new ThreadStart(erunnable.run));
+                threads[i] = t;
+                t.Start();
+            }
+
+            BigInteger sum = new BigInteger(0);
+
+            for (int i = 0; i < threads_count; i++)
+            {
+                threads[i].Join();
+                sum = BigInteger.Add(sum, results[i]);
+            }
+
+            BigInteger degree = BigInteger.Pow(new BigInteger(10), precision);
+            string devider = BigInteger.Divide(sum, degree).ToString();
+            string remainder = BigInteger.Remainder(sum, degree).ToString();
+            while (remainder.Length < precision)
+            {
+                remainder = "0" + remainder;
+            }
+            string neper = string.Format("{0}.{1}", devider, remainder);
+            watch.Stop();
+
+            Console.WriteLine();
+            Console.WriteLine("Final Result: " + neper);
+            Console.WriteLine("Elapsed Time : " + (watch.ElapsedMilliseconds) + " milliseconds");
+
+
+            if (!string.IsNullOrEmpty(filePath))
+            {
+                writeToFile("Final Result:" + neper);
+                writeToFile("Elapsed Time : " + (watch.ElapsedMilliseconds) + " milliseconds");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void writeToFile(string content)
+        {
+            File.AppendAllLines(filePath + ".txt", new string[] { "\n" + content });
+        }
+    }
+
+}
